a2bdd4ba98b819dd9e3fbfa72361c965b735bbf3
leriaetnasta <61632665+leriaetnasta@users.noreply.github.com>
2021-02-08 21:20:02 +0100

    Add files via upload

--- tp_db/tp_personne.php
+++ tp_db/tp_personne.php
@@ -1,1 +1,17 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    $pdo= new pdo("mysql:host=localhost;dbname=mabase","root","");
+    $stm= $pdo->prepare("insert into personne(nom,prenom,age) 
+    values(:nom,:prenom,:age)");
+    $stm->execute([":nom"=>"talha",":prenom"=>"hamza",":age"=>21]);
+    
+    ?>
+</body>
+</html>
